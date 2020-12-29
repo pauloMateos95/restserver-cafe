1a0b6bf0910ce987aed472b283a5083d42d0f981
Paulo Mateos <paulomateos95@gmail.com>
2020-12-28 20:44:04 -0600

    Cambios ENV

--- server/config/config.js
+++ server/config/config.js
@@ -20,6 +20,6 @@ let urlDB;
 if( process.env.NODE_ENV === 'dev' ) {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://paulo:P7121995p@cluster0.wwwsc.mongodb.net/';
+    urlDB = process.env.MONGO_URI;
 }
 process.env.URLDB = urlDB;
